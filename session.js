70f775d83c6421be881c3b64d6e4030ab998e0a9
raddadrob <36490526+raddadrob@users.noreply.github.com>
2024-05-23 13:46:32 -0700

    Create session.js

---
+++ session.js
@@ -0,0 +1,31 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDy0PPpo6xMukyahC4DBCQ_ILJNROdvPWM",
+    authDomain: "cockrumfinance.firebaseapp.com",
+    projectId: "cockrumfinance",
+    storageBucket: "cockrumfinance.appspot.com",
+    messagingSenderId: "446623626941",
+    appId: "1:446623626941:web:3dc1d2e435caf61d04e9d4",
+    measurementId: "G-JFRC4K0BBF"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+document.addEventListener('DOMContentLoaded', () => {
+    auth.onAuthStateChanged((user) => {
+        if (!user) {
+            window.location.href = 'login.html';
+        } else {
+            sessionStorage.setItem('userLoggedIn', 'true');
+        }
+    });
+
+    window.addEventListener('beforeunload', () => {
+        auth.signOut().then(() => {
+            sessionStorage.removeItem('userLoggedIn');
+        }).catch((error) => {
+            console.error('Error signing out:', error);
+        });
+    });
+});
